fb8357b9897e5e189c9c314f9c7056da25ae7b81
kevin <test@test.se>
2024-09-09 20:44:59 +0200

    fix customData RF

--- Provision_scripts/Cloud-init-RF.sh
+++ Provision_scripts/Cloud-init-RF.sh
@@ -10,15 +10,16 @@ rm /etc/nginx/sites-available/default
 cat <<EOF > /etc/nginx/sites-available/default
 server {
   listen        80 default_server;
+
   location / {
     proxy_pass         http://10.0.0.100:5000/;
     proxy_http_version 1.1;
-    proxy_set_header   Upgrade $http_upgrade;
-    proxy_set_header   Connection keep-alive;
-    proxy_set_header   Host $host;
-    proxy_cache_bypass $http_upgrade;
-    proxy_set_header   X-Forwarded-For $proxy_add_x_forwarded_for;
-    proxy_set_header   X-Forwarded-Proto $scheme;
+    proxy_set_header   Upgrade "$http_upgrade";
+    proxy_set_header   Connection "keep-alive";
+    proxy_set_header   Host "$host";
+    proxy_cache_bypass "$http_upgrade";
+    proxy_set_header   X-Forwarded-For "$proxy_add_x_forwarded_for";
+    proxy_set_header   X-Forwarded-Proto "$scheme";
   }
 }
 EOF
